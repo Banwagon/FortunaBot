6641f7f5e458bf625fce6df1c823b5ec4882600d
Banwagon <81770675+Banwagon@users.noreply.github.com>
2021-09-03 18:18:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import asyncio
 from discord.ext import commands
 #from keep_alive import keep_alive
 #from MaxEmbeds import EmbedBuilder
-from boto.s3.connection import S3Connection
+#from boto.s3.connection import S3Connection
 
 #keep_alive()  #script that will ping server
 client = commands.Bot(command_prefix='$')  #sets command prefix as $
